177efb61c721168b92a0b41a6af782ffb1e9925c
NathanKhadaroo <55386091+NathanKhadaroo@users.noreply.github.com>
2019-12-10 18:47:05 +0000

    Added a more robust input which runs a default number of iterations if a wrong value is entered as well as more detailed comments

--- Drunks_go_home.py
+++ Drunks_go_home.py
@@ -6,59 +6,79 @@ Created on Mon Dec  2 15:19:53 2019
 """
 
 #Importing packages
+
 import matplotlib.animation
 import matplotlib.pyplot as plt
 import pandas
 import agentframework_drinkers
 import csv
 import sys
-#defining the nuber of iterations
 
-num_of_iterations = int(sys.argv[1])
+
+#takes the argument given when running the code
+#forces it to be an integer, otherwise returns a value error
+#defines the result as the number of interations
+
+
+while True:
+    try:
+       num_of_iterations = int(sys.argv[1])
+       break
+    except ValueError:
+        print("Invalid entry, running model with default value of 100") 
+        num_of_iterations = 100
+        break
+#num_of_iterations = int(sys.argv[1])
 
 #creates the environment from the raster file
 
-
+#creates an empty list
 environment = []
 
-
+#uses the built in open function to read the raster file
 
 with open('drunk.plan.txt') as f:
     for row in f:
+        #breaks down rown in the text file into smaller chunks
         rowlist = row.split(',')
         rowlist_of_lists = [int(x) for x in rowlist]
+        #appends the integer values to the empty list 'environment'
         environment.append(rowlist_of_lists)
-        
+
+#creates an integer variable for the dimensions of the environment        
 env_limits = len(environment)  
 
 """
-#Plots the bare environment to tests whether the environment is read correctly
+#visualises the environment in pyplot to test whether the environment is read correctly
 
 plt.xlim(0, env_limits)
 plt.ylim(0, env_limits)
 plt.imshow(environment)
 
 """
-#Creates the pubs 
+#Creating the pub
 
-#reads in the raster file a dataframe
+#using pandas' read_csv function creates a dataframe called pubfinder from the raster file
 pubfinder = pandas.read_csv('drunk.plan.txt')
 
 
-#creates lists for the vertical and horizontal dimesnions of the pub
+#creates two empty lists for the vertical and horizontal dimesnions of the pub
 pubx = []
 puby = []
 
-#checks the all coordinates for value of 1, if a one is found appends the
-#coordinates to pubx and puby lists
+
+
+#cycles through every variable in the dataframe
 for row in range(len(pubfinder)): 
          for col in range(len(pubfinder)):
+             #checks the all coordinates for values of 1, if one is found appends the
+             #row and column index of the point to the pubx and puby lists respectively
              if pubfinder.iat[row,col] == 1:
                  pubx.append(row)
                  puby.append(col)
 
 """
-#Checks that the pub has been correctly identified by plotting it 
+#Checks that the pub has been correctly identified by plotting it with pyplot
 
 plt.xlim(0, env_limits)
 plt.ylim(0, env_limits)
@@ -67,20 +87,21 @@ plt.scatter(pubx, puby)
 plt.show()       
 """
 
-
-#Create the drinkers and assigns them a house 
-agents = [] 
-
+#creates a list containing all the "adresses" of the houses 
 adresses = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110,\
           120, 130, 140, 150, 160, 170, 180, 190, 200, 210, 220, 230, 240, 250]
 
+#creates an empty list for our agents
+agents = [] 
+
+#calls the agent framework and creates a list of agent objects who each have
+#a location in the environment, specifically in the pub, and a unique "adress" 
 for i in range (25):
     agents.append(agentframework_drinkers.Agent(environment, agents,\
                                                 pubx,puby, adresses[i]))
 
-
 """    
-#plots the drinkers, to test that they have been created correctly
+#plots the drinkers and the environment, to test that they have been created correctly
 
 plt.xlim(0, env_limits)
 plt.ylim(0, env_limits)
@@ -89,58 +110,78 @@ for agent in agents:
     plt.scatter(agent.x, agent.y, c = 'snow')    
 """ 
 
-#animation time
 
+
+#Animating the model
+
+#creates a 5 by 5 figure object
 fig = plt.figure(figsize=(5, 5))
 
+#creates a set with the adresses as this will be faster
+#than a list for checking if an agent is at a house
 adresses_set = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110,\
           120, 130, 140, 150, 160, 170, 180, 190, 200, 210, 220, 230, 240, 250}
 
+
+
 def update(frame_number):
     
-    
-    fig.clear()  
+    #clears the environment from the last iteration
+    fig.clear() 
+    #plots the environment
     plt.imshow(environment)
     plt.xlim(0, agents[0].env_limits-1)
     plt.ylim(0, agents[0].env_limits)
+    #plots the "pub"
     plt.scatter(pubx, puby)
  
     
     for agent in agents:  
         
+        #calls the agent framework and updates 
         agent.move()
         
+        #plots the agent in the environment
         plt.scatter(agent.x, agent.y, c = 'snow')  
         
+        #cheks to see if the agents "adress" corresponds to its location
         if pubfinder.iat[agent.x, agent.y] == agent.adresses:
-           
+            #if above is TRUE, remouves the agent from teh simulation and prints a message 
             agents.remove(agent)
             print("The person who lives at house number", agent.adresses,"has found their way home")
         
+        #calls the agent framework to update the environment  
         agent.leave_footprint(pubfinder, adresses_set)
             
            
     #print(len(agents))
-    print(frame_number)
+   
+    #print(frame_number)
+    
+    #prints a message if all the agents find their way home
     if len(agents) == 0:
-        print("Everybody found their way home.")
-        
+        print("Everybody found their way home!")
+    
+    #checks how many agents aren't "home" (len(agents))at the end of 
+    #the model and prints an appropiate message  
     if frame_number == num_of_iterations-1:
         if len(agents) == 25:
             print ("Nobody found their way home.")
+        elif len(agents) == 24:
+            print("Only 1 person found their way home")
         else:
             print("Only", 25-len(agents), "people found their way home.")
     
-        
+#defines the parameters of our animation         
 animation = matplotlib.animation.FuncAnimation(fig, update, interval=1, repeat=False, frames=num_of_iterations)
 
+#looks for a fig object and opens the window in which it visualises it
 plt.show()
 
-for i in range (len(environment)):
-    environment[i]= environment[i]*1
-
+#creates a new file csv file called density_map.txt
 with open('density_map.txt', 'w', newline='') as output:
             csvwriter = csv.writer(output, delimiter=',',
                                    quoting=csv.QUOTE_MINIMAL)
+            #writes the density file by printing the environment printing row by row
             for row in environment:
                 csvwriter.writerow(row)
--- agentframework_drinkers.py
+++ agentframework_drinkers.py
@@ -4,7 +4,7 @@ Created on Wed Dec  4 11:15:51 2019
 
 @author: msra2nk3
 """
-
+#imports packages
 import random
 
 
@@ -16,18 +16,20 @@ class Agent():
          
          self.environment = environment
          self.env_limits = len(environment)
+        
          #initialies the starting coordinates of the drunks somewhere in the pub
          self.pubx = pubx
          self.puby = puby
          self.x = random.randrange (min(pubx), max(pubx)+1, 1)
          self.y = random.randrange (min(puby), max(puby)+1, 1)
+         
+         
          self.agents = agents
-         #assigns a home to each agent
          self.adresses = adresses
          
      
     def move(self):
-        #randomly moves agents by 7 steps in both dimensions
+        #randomly moves agents by 5 steps in both dimensions
             if random.random() < 0.5:
                 self.y = (self.y + 5)
             else:
@@ -38,7 +40,7 @@ class Agent():
             else:
                 self.x = (self.x - 5)
        #checks to see if the agents movement pushes them off the border of the
-       #environment, if it does it instead outputs their location as at the border
+       #environment, if it does it instead outputs their location as being at the border
             if self.x < 0:
                 self.x = 0
             
@@ -55,7 +57,8 @@ class Agent():
         #this ensures that the houses are not renamed if agents pass through them 
         if pubfinder.iat[self.x, self.y] in adresses_set :
             pass
-       
+        
+        #reduces the environment value of the point at which the agent is
         else:
             self.environment[self.y][self.x] -= 1
         
